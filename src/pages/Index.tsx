ce6e9e206168cb64faa29ce8bc7ba8fbd8e28af3
Gabby-Tech1 <gkaquainoo@gmail.com>
2025-08-13 13:29:43 +0000

    Project setup

---
+++ src/pages/Index.tsx
@@ -0,0 +1,11 @@
+// Update this page (the content is just a fallback if you fail to update the page)
+
+const Index = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-background">
+      <p>Home Page</p>
+    </div>
+  );
+};
+
+export default Index;
