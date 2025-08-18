3ad55bccfa36e6acf18f1fe3ef9042d27ccff62e
ephraim64 <tettehephraim.64@gmail.com>
2025-08-18 07:49:47 +0000

    landing page completed

--- src/assets/assets.jsx
+++
@@ -1,9 +0,0 @@
-import location from './akar-icons_location.png';
-import premuim from './fluent_premium-20-regular.png';
-
-const assets = {
-   location,
-   premuim
-}
-
-export default assets;
---
+++ src/assets/assets.tsx
@@ -0,0 +1,37 @@
+import location from './akar-icons_location.png';
+import premuim from './fluent_premium-20-regular.png';
+import solarBox from './solar_box-linear.png'
+import grommet from './grommet-icons_status-good.png'
+import heroImage from './heroImage.png'
+import moneyicon from './hugeicons_money-bag-02.png'
+import packageicon from './Icon.png'
+import deliveryTrack from './image 12.png'
+import logo from './logo.svg'
+import map from './Map.png'
+import materialSymbol from './material-symbols_verified-user-outline-rounded.png'
+import deliveryTrack3 from './pngegg (12).png'
+import deliveryTrack2 from './pngwing.com (25) 1.png'
+import trackIcon from './hugeicons_truck.png'
+
+
+
+
+const assets = {
+   location,
+   premuim,
+   solarBox,
+   grommet,
+   heroImage,
+   moneyicon,
+   packageicon,
+   deliveryTrack,
+   logo,
+   map,
+   materialSymbol,
+   deliveryTrack3,
+   deliveryTrack2,
+   trackIcon
+
+}
+
+export default assets;
